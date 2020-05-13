72b646dc4d05e44144d00afc12e2f99425701acc
Rodrigo Alex <rodrigo.alex.sousa@gmail.com>
2020-05-13 09:54:24 -0300

    User insert

--- src/main/java/com/educandoweb/course/entities/enums/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/enums/OrderItem.java
@@ -50,6 +50,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
